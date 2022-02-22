bf1387edfd3e5fe26d1506e115f483b07408a509
Manu200162 <manli200134@gmail.com>
2022-02-22 08:30:31 -0400

    Commit3

--- src/test/java/ejercicioExtra/BecaEstudioTest.java
+++ src/test/java/ejercicioExtra/BecaEstudioTest.java
@@ -1,7 +1,10 @@
 package ejercicioExtra;
 
+import ejercicio_extra.BecaEstudio;
 import ejercicio_extra.Helpers;
 import ejercicio_extra.Utils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
@@ -10,10 +13,27 @@ public class BecaEstudioTest {
 
     Utils utilsMock= Mockito.mock(Utils.class);
 
+    @BeforeAll
+    public static void before(){
+        MockedStatic<Helpers> helpersMock= Mockito.mockStatic(Helpers.class);
+        helpersMock.when(()->Helpers.aplicaBeca(90)).thenReturn(false);
+    }
+
+    @Test
+    public void verifygetNotaFalse(){
+        MockedStatic<Helpers> helpersMock= Mockito.mockStatic(Helpers.class);
+        helpersMock.when(()->Helpers.aplicaBeca(78)).thenReturn(false);
+        BecaEstudio beca=new BecaEstudio();
+        String expected="EL ESTUDIANTE NO CURSO AUN EL 60% DE LAS MATERIAS";
+        String actual= beca.getResultBeca(78);
+        Assertions.assertEquals(expected,actual);
+    }
+
     @Test
     public void verifyBeca(){
-        MockedStatic<Helpers> helpersMock= Mockito.mockStatic(Helpers.class);
         Mockito.when(utilsMock.getNota(78)).thenReturn(92);
+        BecaEstudio beca=new BecaEstudio();
+
 
     }
 }
